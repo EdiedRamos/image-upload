ad04b02036d11ef45302b4b53af46c555266c6c8
EdiedRamos <edieisa22@gmail.com>
2024-06-10 21:20:51 -0500

    style: set responsive header padding

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -3,7 +3,7 @@ import { ThemeToggle } from "@/components";
 
 export const Header = () => {
   return (
-    <header className="bg-cc-ghost-white dark:bg-cc-midnight-blue  flex flex-wrap items-center justify-between px-[72px] py-4 border-b-[1px]">
+    <header className="bg-cc-ghost-white dark:bg-cc-midnight-blue  flex flex-wrap items-center justify-between px-4 sm:px-5 md:px-[72px] py-4 border-b-[1px]">
       <Logo className="text-cc-eerie-black dark:text-cc-ghost-white" />
       <ThemeToggle />
     </header>
