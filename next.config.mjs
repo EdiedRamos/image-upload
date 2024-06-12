b43d264eabe7b46271f8abc673fbd266d1545e94
EdiedRamos <edieisa22@gmail.com>
2024-06-12 18:31:27 -0500

    chore: continue without logs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: "https://image-upload-azure.vercel.app/",
+    },
+  },
+};
 
 export default nextConfig;
