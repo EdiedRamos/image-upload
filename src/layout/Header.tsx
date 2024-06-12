5ce7f007762a0cf31e7f97c55f926b776538575c
EdiedRamos <edieisa22@gmail.com>
2024-06-12 14:48:01 -0500

    feat: add redirect to /uploader with the app icon

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,10 +1,13 @@
+import Link from "next/link";
 import { Logo } from "@/images";
 import { ThemeToggle } from "@/components";
 
 export const Header = () => {
   return (
     <header className="dark:border-b-cc-outer-space-light  flex flex-wrap items-center justify-between px-4 sm:px-5 md:px-[72px] py-4 border-b-[1px]">
-      <Logo className="text-cc-eerie-black dark:text-cc-ghost-white" />
+      <Link href="/uploader">
+        <Logo className="text-cc-eerie-black dark:text-cc-ghost-white" />
+      </Link>
       <ThemeToggle />
     </header>
   );
